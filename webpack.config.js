00d8fb0a031c6b74c486c4afb51a87cc4ba48c78
alexlee-dev <alex@alexlee.dev>
2020-04-27 21:13:48 -0700

    ⚛️ Add React

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const webpack = require("webpack");
+const chalk = require("chalk");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const outputDirectory = "dist";
+const webpackMode =
+  process.env.NODE_ENV === "production" ? "production" : "development";
+
+console.log(`Webpack Mode === ${chalk.yellowBright(webpackMode)}\n`);
+
+module.exports = {
+  mode: webpackMode,
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    historyApiFallback: true,
+    proxy: {
+      "*": "http://localhost:3000",
+    },
+  },
+  devtool: "source-map",
+  entry: ["./src/client/index.tsx"],
+  output: {
+    path: path.join(__dirname, outputDirectory),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        use: ["@svgr/webpack", "url-loader"],
+      },
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader",
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf)$/,
+        loader: "url-loader?limit=100000",
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader",
+      },
+    ],
+  },
+  // When importing a module whose path matches one of the following, just
+  // assume a corresponding global variable exists and use that instead.
+  // This is important because it allows us to avoid bundling all of our
+  // dependencies, which allows browsers to cache those libraries between builds.
+  // externals: {
+  //   react: "React",
+  //   "react-dom": "ReactDOM",
+  // },
+};
